bd32cb8f421f42b7ae958ad35d6df30ec209393e
Francois Valadier <francois.valadier@gmail.com>
2022-05-04 17:40:39 +0200

    refacto & improve docs

--- src/azure_blobstorage_utils/base.py
+++ src/azure_blobstorage_utils/base.py
@@ -1,9 +1,9 @@
-from azure.core.exceptions import ResourceExistsError
+from azure.core.exceptions import ResourceExistsError, ResourceNotFoundError
 from azure.storage.blob import BlobServiceClient
 from typing import List
 import os
 import shutil
-from typing import Optional, Iterable, Union
+from typing import Optional, Iterable, Union, Tuple
 import json
 
 
@@ -20,66 +20,73 @@ class BlobStorageBase:
         self.create_local_dir(self.local_base_path)
         print("Using path: [{}] as local storage".format(self.local_base_path))
 
-    @staticmethod
-    def create_local_dir(dir_path: str):
+    @classmethod
+    def create_local_dir(cls, directory_name: str):
         """
+        Creates a local directory
 
         Args:
-            dir_path:
+            directory_name: directory path
 
         Returns:
 
         """
-        os.makedirs(dir_path, exist_ok=True)
+        os.makedirs(directory_name, exist_ok=True)
 
     @staticmethod
-    def get_folder_and_filename_from_full_path(filename: str):
+    def get_directory_and_filename_from_full_path(file_name: str) -> Tuple[str]:
         """
-
+        Split file_name and get folder_name and "real" file_name
         Args:
-            filename:
+            file_name:
 
-        Returns:
+        Returns: directory_name, file_name
 
         """
-        folder_name = None
-        filename_split = filename.split("/")
+        directory_name = None
+        filename_split = file_name.split("/")
         if len(filename_split) > 1:
-            folder_name = "/".join(filename_split[:-1]) + "/"
-            filename = filename_split[-1]
-        return folder_name, filename
+            directory_name = "/".join(filename_split[:-1]) + "/"
+            file_name = filename_split[-1]
+        return directory_name, file_name
 
     def get_container_client(self, container_name: str):
         """
+        Get container client from container_name
 
         Args:
-            container_name:
+            container_name: Name of the container
 
         Returns:
 
         """
-        return self.blob_service_client.get_container_client(container_name)
+        container_client = self.blob_service_client.get_container_client(container_name)
+        if container_client.exists():
+            return container_client
+        else:
+            raise ResourceNotFoundError("Container [{}] does not exist.".format(container_name))
 
     def create_container(self, container_name: str):
         """
+        Create a container named container_name
 
         Args:
-            container_name:
+            container_name: Name of the container
 
         Returns:
 
         """
         try:
             self.blob_service_client.create_container(name=container_name)
-        except ResourceExistsError:
-            print("Container [{}] already exists. Skipping creation".format(container_name))
+        except ResourceExistsError("Container [{}] already exists. Skipping creation".format(container_name)):
             pass
 
     def delete_container(self, container_name: str):
         """
+        Delete container named container_name
 
         Args:
-            container_name:
+            container_name: Name of the container
 
         Returns:
 
@@ -88,10 +95,11 @@ class BlobStorageBase:
 
     def get_blob_client(self, container_name: str, blob_name: str):
         """
+        Get blob client from container_name & blob_name
 
         Args:
-            container_name:
-            blob_name:
+            container_name: Name of the container
+            blob_name: Name of the blob
 
         Returns:
 
@@ -101,6 +109,7 @@ class BlobStorageBase:
 
     def clean_local_folder(self):
         """
+        Empty & delete local_folder (ie. local_base_path)
 
         Returns:
 
@@ -109,10 +118,11 @@ class BlobStorageBase:
 
     def get_file_as_bytes(self, container_name: str, remote_file_name: str) -> bytes:
         """
+        Get blob as bytes (in memory object)
 
         Args:
-            container_name:
-            remote_file_name:
+            container_name: Name of the container
+            remote_file_name: Name of the blob
 
         Returns:
 
@@ -122,10 +132,11 @@ class BlobStorageBase:
 
     def get_file_as_text(self, container_name: str, remote_file_name: str) -> str:
         """
+        Get blob as text
 
         Args:
-            container_name:
-            remote_file_name:
+            container_name: Name of the container
+            remote_file_name: Name of the blob
 
         Returns:
 
@@ -134,104 +145,104 @@ class BlobStorageBase:
 
     def get_file_as_dict(self, container_name: str, remote_file_name: str) -> str:
         """
+        Get blob as dict (eg. for JSON files)
 
         Args:
-            container_name:
-            remote_file_name:
+            container_name: Name of the container
+            remote_file_name: Name of the blob
 
         Returns:
 
         """
         return json.loads(self.get_file_as_text(container_name, remote_file_name))
 
-    def get_list_blobs_name(self, container_name: str, prefix: Optional[str] = None, return_list: bool = True) \
+    def get_list_blobs_name(self, container_name: str, prefix: Optional[str] = None, return_list: Optional[bool] = True) \
             -> Union[List[str], Iterable[str]]:
         """
+        Get list/generator of objects in container_name
 
         Args:
-            container_name:
-            prefix:
-            return_list:
+            container_name: Name of the container
+            prefix: filter blobs with prefix
+            return_list: if False, returns a generator
 
         Returns:
 
         """
         container_client = self.get_container_client(container_name)
-        if container_client.exists():
-            if prefix is not None:
-                res = (blob.name for blob in container_client.list_blobs(name_starts_with=prefix))
-            else:
-                res = (blob.name for blob in container_client.list_blobs())
-
-            if return_list:
-                res = list(res)
-            return res
-
+        if prefix is not None:
+            res = (blob.name for blob in container_client.list_blobs(name_starts_with=prefix))
         else:
-            print("Container [{}] doesn't exists".format(container_name))
-        return None
+            res = (blob.name for blob in container_client.list_blobs())
 
-    def download_file(self, container_name: str, remote_file_name: str, local_file_name: str = None):
+        if return_list:
+            res = list(res)
+        return res
+
+    def download_file(self, container_name: str, remote_file_name: str, local_file_name: Optional[str] = None):
         """
+        Download a blob named remote_file_name from a container named container_name
 
         Args:
-            container_name:
-            remote_file_name:
-            local_file_name:
+            container_name: Name of the container
+            remote_file_name: Name of the blob
+            local_file_name: Name of the local file where the blob will be downloaded
 
         Returns:
 
         """
         if local_file_name is None:
-            foldername, filename = self.get_folder_and_filename_from_full_path(remote_file_name)
-            if foldername is None:
+            directory_name, file_name = self.get_directory_and_filename_from_full_path(remote_file_name)
+            if directory_name is None:
                 self.create_local_dir(self.local_base_path)
-                local_file_name = self.local_base_path + filename
+                local_file_name = self.local_base_path + file_name
             else:
-                self.create_local_dir(self.local_base_path + foldername)
-                local_file_name = self.local_base_path + foldername + filename
+                self.create_local_dir(self.local_base_path + directory_name)
+                local_file_name = self.local_base_path + directory_name + file_name
         else:
-            foldername, filename = self.get_folder_and_filename_from_full_path(local_file_name)
-            if foldername is None:
+            directory_name, file_name = self.get_directory_and_filename_from_full_path(local_file_name)
+            if directory_name is None:
                 self.create_local_dir(self.local_base_path)
-                local_file_name = self.local_base_path + filename
+                local_file_name = self.local_base_path + file_name
             else:
-                self.create_local_dir(foldername)
-                local_file_name = foldername + filename
+                self.create_local_dir(directory_name)
+                local_file_name = directory_name + file_name
 
         blob_client = self.get_blob_client(container_name, remote_file_name)
         print("Downloading {} to {}".format(remote_file_name, local_file_name))
         with open(local_file_name, "wb") as my_blob:
             my_blob.write(blob_client.download_blob().readall())
 
-    def download_folder(self, container_name: str, remote_folder: str, local_folder: Optional[str] = None):
+    def download_directory(self, container_name: str, remote_directory: str, local_directory: Optional[str] = None):
         """
+        Download all blobs in directory
 
         Args:
-            container_name:
-            remote_folder:
-            local_folder:
+            container_name: Name of the container
+            remote_directory: Name of the remote directory
+            local_directory: Name of the local directory where files will be downloaded
 
         Returns:
 
         """
-        blob_gen = self.get_list_blobs_name(container_name, prefix=remote_folder, return_list=False)
+        blob_gen = self.get_list_blobs_name(container_name, prefix=remote_directory, return_list=False)
 
         for blob_name in blob_gen:
-            if local_folder is not None:
-                self.download_file(container_name, blob_name, (local_folder + blob_name).replace("//", "/"))
+            if local_directory is not None:
+                self.download_file(container_name, blob_name, (local_directory + blob_name).replace("//", "/"))
             else:
                 self.download_file(container_name, blob_name)
 
     def upload_file(self, container_name: str, local_file_name: str, remote_file_name: Optional[str] = None,
-                    overwrite: bool = False):
+                    overwrite: Optional[bool] = False):
         """
+        Upload a local file to a blob
 
         Args:
-            container_name:
-            local_file_name:
-            remote_file_name:
-            overwrite:
+            container_name: Name of the container
+            local_file_name: Name of the local file
+            remote_file_name: Name of the blob where file will be uploaded
+            overwrite: set to True if needed
 
         Returns:
 
@@ -242,66 +253,70 @@ class BlobStorageBase:
             container_client = self.get_container_client(container_name)
 
         if remote_file_name is None:
-            foldername, filename = self.get_folder_and_filename_from_full_path(local_file_name)
-            remote_file_name = foldername.replace(self.local_base_path, "") + filename
+            directory_name, file_name = self.get_directory_and_filename_from_full_path(local_file_name)
+            remote_file_name = directory_name.replace(self.local_base_path, "") + file_name
 
         blob_client = container_client.get_blob_client(remote_file_name)
         try:
             with open(local_file_name, "rb") as data:
                 blob_client.upload_blob(data, overwrite=overwrite)
-        except ResourceExistsError:
-            print("File [{}] already exists. Use overwrite = True if needed".format(remote_file_name))
+        except ResourceExistsError("File [{}] already exists. Use overwrite = True if needed".format(remote_file_name)):
             pass
 
-    def _get_filepaths_from_folder(self, directory: str):
+    @classmethod
+    def _get_file_paths_from_directory(cls, directory_name: str):
         """
-
+        Get all file paths from local directory
         Args:
-            directory:
+            directory_name: Name of the local directory
 
         Returns:
 
         """
-        file_paths = []  # List which will store all of the full filepaths.
+        file_paths = []  # List which will store all of the full file paths.
 
         # Walk the tree.
-        for root, directories, files in os.walk(directory):
-            for filename in files:
+        for root, directories, files in os.walk(directory_name):
+            for file_name in files:
                 # Join the two strings in order to form the full filepath.
-                filepath = os.path.join(root, filename)
-                file_paths.append(filepath)  # Add it to the list.
+                file_path = os.path.join(root, file_name)
+                file_paths.append(file_path)  # Add it to the list.
 
         return file_paths
 
-    def upload_folder(self, container_name: str, local_folder_name: str, remote_folder_name: Optional[str] = None,
-                      overwrite: bool = False):
+    def upload_directory(self, container_name: str, local_directory_name: str,
+                         remote_directory_name: Optional[str] = None,
+                         overwrite: Optional[bool] = False):
         """
+        Upload local folder to blobs
 
         Args:
-            container_name:
-            local_folder_name:
-            remote_folder_name:
-            overwrite:
+            container_name: Name of the container
+            local_directory_name: Name of the local directory
+            remote_directory_name: Name of the remote directory where file will be uploaded
+            overwrite: set to True if needed
 
         Returns:
 
         """
-        filepaths = self._get_filepaths_from_folder(local_folder_name)
-        for filepath in filepaths:
-            if remote_folder_name is not None:
-                self.upload_file(container_name, filepath, (remote_folder_name + filepath).replace("//", "/"),
+        file_paths = self._get_file_paths_from_directory(local_directory_name)
+        for filepath in file_paths:
+            if remote_directory_name is not None:
+                self.upload_file(container_name, filepath, (remote_directory_name + filepath).replace("//", "/"),
                                  overwrite)
             else:
                 self.upload_file(container_name, filepath, filepath, overwrite)
 
-    def upload_bytes(self, bytes: bytes, container_name: str, remote_file_name: str, overwrite: bool = False):
+    def upload_bytes(self, my_bytes: bytes, container_name: str, remote_file_name: str,
+                     overwrite: Optional[bool] = False):
         """
+        Uploaded in memory byte object to blob
 
         Args:
-            bytes:
-            container_name:
-            remote_file_name:
-            overwrite:
+            my_bytes: in memory object
+            container_name: Name of the container
+            remote_file_name: Name of the blob where object will be uploaded
+            overwrite: set to True if needed
 
         Returns:
 
@@ -312,20 +327,18 @@ class BlobStorageBase:
             container_client = self.get_container_client(container_name)
 
         blob_client = container_client.get_blob_client(remote_file_name)
-        blob_client.upload_blob(bytes, overwrite=overwrite)
+        blob_client.upload_blob(my_bytes, overwrite=overwrite)
 
     def delete_blobs(self, container_name: str, remote_file_names: List[str]):
         """
+        Delete list of blobs from container_name
 
         Args:
-            container_name:
-            remote_file_names:
+            container_name: Name of the container
+            remote_file_names: list of blob names
 
         Returns:
 
         """
         container_client = self.get_container_client(container_name)
-        if not container_client.exists():
-            pass
-        else:
-            container_client.delete_blobs(*remote_file_names)
+        container_client.delete_blobs(*remote_file_names)
--- src/azure_blobstorage_utils/extended.py
+++ src/azure_blobstorage_utils/extended.py
@@ -2,6 +2,7 @@ from .base import BlobStorageBase
 import io
 import sys
 import os
+from typing import Dict, Optional
 
 try:
     import pandas as pd
@@ -22,58 +23,62 @@ class BlobStorageExtended(BlobStorageBase):
         """
 
         Args:
-            connection_string:
-            local_base_path:
+            connection_string: Connection string to Azure Blob Storage
+            local_base_path: local folder where data will be downloaded if path is not specified
         """
         super().__init__(connection_string, local_base_path)
 
-    def get_file_as_pandas_df(self, container_name: str, file_path: str, **kwargs) -> pd.DataFrame:
+    def get_file_as_pandas_df(self, container_name: str, remote_file_name: str,
+                              **kwargs: Optional[Dict]) -> pd.DataFrame:
         """
+        Get a blob & load it as a pandas DataFrame
 
         Args:
-            container_name:
-            file_path:
-            **kwargs:
+            container_name: Name of the container
+            remote_file_name: Name of the blob
+            **kwargs: add any kwarg that you would put in pd.read_* methods.
 
-        Returns:
+        Returns: a pandas DataFrame
 
         """
-        stream = self.get_file_as_bytes(container_name, file_path)
-        if file_path.endswith(".csv") | file_path.endswith(".txt"):
+        stream = self.get_file_as_bytes(container_name, remote_file_name)
+        if remote_file_name.endswith(".csv") | remote_file_name.endswith(".txt"):
             return pd.read_csv(io.BytesIO(stream), **kwargs)
-        elif file_path.endswith(".parquet"):
+        elif remote_file_name.endswith(".parquet"):
             return pd.read_parquet(io.BytesIO(stream), **kwargs)
-        elif file_path.endswith(".json"):
+        elif remote_file_name.endswith(".json"):
             return pd.read_json(io.BytesIO(stream), **kwargs)
-        elif file_path.endswith(".xls") | file_path.endswith(".xlsx"):
+        elif remote_file_name.endswith(".xls") | remote_file_name.endswith(".xlsx"):
             return pd.read_excel(io.BytesIO(stream), **kwargs)
         else:
             raise ValueError(
                 "Extension not recognized - only ['csv','txt','parquet','json','xls','xlsx'] are supported.")
 
-    def get_image_as_numpy_array(self, container_name: str, file_path: str) -> np.ndarray:
+    def get_image_as_numpy_array(self, container_name: str, remote_file_name: str) -> np.ndarray:
         """
+        Get an image file from blob & load it as numpy array
 
         Args:
-            container_name:
-            file_path:
+            container_name: Name of the container
+            remote_file_name: Name of the blob
 
-        Returns:
+        Returns: a RGB numpy array of the image
 
         """
-        stream = self.get_file_as_bytes(container_name, file_path)
+        stream = self.get_file_as_bytes(container_name, remote_file_name)
         img = cv2.imdecode(np.frombuffer(stream, np.uint8), cv2.IMREAD_COLOR)
         return img
 
-    def upload_image_bytes_as_jpg_file(self, img: bytes, container_name: str, remote_file_name: str,
-                                       overwrite: bool = False):
+    def upload_image_bytes_as_jpg_file(self, img: np.ndarray, container_name: str, remote_file_name: str,
+                                       overwrite: Optional[bool] = False):
         """
+        Upload an in memory image numpy array as a jpg file
 
         Args:
-            img:
-            container_name:
-            remote_file_name:
-            overwrite:
+            img: a RGB numpy array
+            container_name: Name of the container
+            remote_file_name: Name of the blob where image will be uploaded
+            overwrite: set to True if needed
 
         Returns:
 
@@ -82,21 +87,22 @@ class BlobStorageExtended(BlobStorageBase):
         img_bytes = img_encode.tobytes()
         self.upload_bytes(img_bytes, container_name, remote_file_name, overwrite)
 
-    def upload_pandas_df(self, df: pd.DataFrame, container_name: str, remote_file_name: str, overwrite: bool = False,
-                         **kwargs) -> pd.DataFrame:
+    def upload_pandas_df(self, df: pd.DataFrame, container_name: str, remote_file_name: str,
+                         overwrite: Optional[bool] = False,
+                         **kwargs: Optional[Dict]) -> pd.DataFrame:
         """
-
+        Upload a in memory pandas DataFrame to a blob
         Args:
-            df:
-            container_name:
-            remote_file_name:
-            overwrite:
-            **kwargs:
+            df: a pandas DataFrame
+            container_name: Name of the container
+            remote_file_name: Name of the blob where the dataframe will be uploaded
+            overwrite: set to True if needed
+            **kwargs: add any kwarg that you would put in pd.to_* methods.
 
         Returns:
 
         """
-        foldername, filename = self.get_folder_and_filename_from_full_path(remote_file_name)
+        foldername, filename = self.get_directory_and_filename_from_full_path(remote_file_name)
         if filename.endswith(".csv") | filename.endswith(".txt"):
             df.to_csv(self.local_base_path + filename, **kwargs)
         elif remote_file_name.endswith(".parquet"):
